6e62d89195cdf772958c52fcd6f772e2eed7f0e0
Nadezhda Martin <nadej-martin@yandex.ru>
2025-08-20 14:33:05 +0300

    feat: ready table

--- src/components/filtering.js
+++ src/components/filtering.js
@@ -1,14 +1,48 @@
 import {createComparison, defaultRules} from "../lib/compare.js";
 
 // @todo: #4.3 — настроить компаратор
+const compare = createComparison(defaultRules);
 
 export function initFiltering(elements, indexes) {
     // @todo: #4.1 — заполнить выпадающие списки опциями
+    Object.keys(indexes)                                    // Получаем ключи из объекта
+        .forEach((elementName) => {                        // Перебираем по именам
+            elements[elementName].append(                    // в каждый элемент добавляем опции
+                ...Object.values(indexes[elementName])        // формируем массив имён, значений опций
+                        .map(name => {                        // используйте name как значение и текстовое содержимое
+                            // Создаем элемент option
+                            const option = document.createElement('option');
 
+                            // Устанавливаем значение атрибута value
+                            option.value = name;
+
+                            // Устанавливаем текст внутри тега option
+                            option.textContent = name;       // @todo: создать и вернуть тег опции
+                        
+                            return option;
+                        })
+            )
+    })
     return (data, state, action) => {
         // @todo: #4.2 — обработать очистку поля
+        const buttons = document.querySelectorAll('button');
+        for (let i = 0; i < buttons.length; i++) {
+            const button = buttons[i];
+            const name = button.getAttribute('name');
+            if (name && name.includes('clear')) {
+                const input = button.closest('.filter-wrapper').querySelector('input'); // замените '.parent-class' на класс родительского элемента
+                if (input) {
+                    input.value = ''; // Сброс значения input
+                    
+                    // Получаем имя поля из атрибута data-field
+                    const fieldName = button.dataset.field;
 
+                    // Обновляем соответствующее поле в state
+                    state[fieldName] = '';
+                }
+            }
+        }
         // @todo: #4.5 — отфильтровать данные используя компаратор
-        return data;
+        return data.filter(row => compare(row, state));
     }
 }
--- src/components/table.js
+++ src/components/table.js
@@ -12,14 +12,54 @@ export function initTable(settings, onAction) {
     const root = cloneTemplate(tableTemplate);
 
     // @todo: #1.2 —  вывести дополнительные шаблоны до и после таблицы
+     before.reverse().forEach(templateId => {//переворачивает массив.перебираем массив идентификаторов
+        root[templateId] = cloneTemplate(templateId);//клонируем и получаем объект, сохраняем в таблице
+        root.container.prepend(root[templateId].container); // добавляет каждый клонированный шаблон перед таблицей
+    });
+
+    after.forEach(templateId => {
+        root[templateId] = cloneTemplate(templateId);
+        root.container.append(root[templateId].container); //добавляет каждый клонированный шаблон из массива after после таблицы
+    });
 
     // @todo: #1.3 —  обработать события и вызвать onAction()
+    root.container.addEventListener('change', () => onAction());
+    root.container.addEventListener('reset', () => {
+        setTimeout(() => {
+            onAction();
+        }, 0);
+    });
+    root.container.addEventListener('submit', (e) => {
+        e.preventDefault();
+        onAction(e.submitter);
+    });
 
     const render = (data) => {
         // @todo: #1.1 — преобразовать данные в массив строк на основе шаблона rowTemplate
-        const nextRows = [];
+        const nextRows = data.map(item => { //создаём новый массив, применяя указанную функцию к каждому элементу item массива data
+            const row = cloneTemplate(rowTemplate) //клонируем шаблон строк
+            
+            Object.keys(item).forEach(key => {//массив ключей объекта item
+                // if (row.elements[key]) {
+                //     row.elements[key].textContent = item[key];
+                // } //Проверяется, существует ли ключ в row.elements. Если да, то значению textContent соответствующего элемента присваивается значение item[key].
+
+                if (key in row.elements) {
+                    const el = row.elements[key];
+                    const tag = el.tagName.toLowerCase();
+                    if (tag === "input" || tag === "select") {
+                        el.value = item[key];
+                    } else {
+                        el.textContent = item[key];
+                    }
+                }
+            });
+            
+        return row.container;
+
+        })
         root.elements.rows.replaceChildren(...nextRows);
     }
-
+        
     return {...root, render};
 }
