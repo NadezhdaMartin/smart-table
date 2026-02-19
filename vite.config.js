250eb3fe600b5e4d51b5dc6683e464b0311ec5d1
Nadezhda Martin <nadej-martin@yandex.ru>
2026-02-19 18:13:50 +0300

    ci: add for deploy

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,5 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  // Замените на имя вашего репозитория, если это не username.github.io
-  // Если это username.github.io, оставьте base: '/' или удалите эту строку
   base: '/smart-table/', 
 })
